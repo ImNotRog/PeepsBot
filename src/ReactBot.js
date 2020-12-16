3914dd6c3cad9f2d47fef07ef0871f9bd65e3c4e
Roger Fan <rogerjoeyfan@gmail.com>
2020-12-16 10:35:31 -0800

    fixing up some new  emotes

--- src/ReactBot.js
+++ src/ReactBot.js
@@ -5,12 +5,12 @@ class ReactBot {
     constructor() {
         this.reactmap = new Map();
         this.reactmap.set("little", "754075455304499211")
-        this.reactmap.set("trg", "776946465780006913")
-        this.reactmap.set("checkpoint", "776946929706729482");
-        this.reactmap.set("cer", "776947387796029451");
+        this.reactmap.set("trg", "788460256052117524")
+        this.reactmap.set("checkpoint", "788460631715348480");
+        this.reactmap.set("cer", "788461210609647646");
         this.reactmap.set("pain", "776522384642932766");
         this.reactmap.set("fperbio", "776522302669062154");
-        this.reactmap.set("hw", "755144784083026101");
+        // this.reactmap.set("hw", "755144784083026101");
         this.reactmap.set("jack", "783125462045032498");
         this.reactmap.set("tired", "783452754625429504");
 
